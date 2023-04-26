4d9c6aa28a510fa69241e1e5bb0d24864a94d499
lucas.freitas <lucas.dellatorre@edu.pucrs.br>
2023-04-26 20:43:27 -0300

    first commit

---
+++ fanwait.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main (int argc, char *argv[]) {
+  pid_t childpid = 0;
+  int i, n;
+
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s processes", argv[0]);
+    return 1;
+  }
+  n = atoi(argv[1]);
+
+  for (i = 1; i < n; i++) 
+    if ((childpid = fork()) <= 0) // only the children enters
+      break;
+
+  fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
+ i, (long)getpid(), (long)getppid(), (long)childpid);
+  while(wait(NULL) > 0) ;
+  
+}
